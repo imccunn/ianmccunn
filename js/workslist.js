222b4ae7e1cd19acd3ccc93b82ec765efc67465f
Ian McCunn <ianmccunn@gmail.com>
2014-09-05 12:58:14 -0700

    Update workslist.js

--- js/workslist.js
+++ js/workslist.js
@@ -14,3 +14,27 @@ $(document).ready(function() {
 		return false;
 	});
 });
+
+	//Handle Workslist Clicks for 'catalog.html' control
+// 	$(document).on('click', '.compExLnk', function() {
+		
+// 		//Find composition opus number, which is also file identifier
+// 		var page = $(this).parent().parent().children('.comp-op').text();
+
+// 		// boolean value for whether specific composition content has been requested and loaded
+// 		var contentLoaded = $('#collapse' + page).data("contentLoaded");
+
+// 		// if content exists on click, empty content and set data atrib to false; otherwise, load the content and set to true
+// 		if (typeof(contentLoaded) == "undefined" && !contentLoaded){
+
+// 			$.ajax('/compositions/op' + page + '.html').done(function(data) {
+// 				$('#collapse' + page).append(data).fadeIn(750);
+// 				$('#collapse' + page).data("contentLoaded", true);
+// 			});
+			
+// 		}
+// 		else $('#collapse' + page).data("contentLoaded", false);
+
+// 		return false;
+// 	});
+// });
